e47a1b495a02f3c40e180ef4313b5b468a32b8a4
ainest312 <ainest.312@icloud.com>
2025-10-23 20:31:15 +0600

    deded

---
+++ firstLesson.py
@@ -0,0 +1,24 @@
+
+# question1=input( "сколько стоит это вешь?")
+# print("это вешь стоит-", question1)
+# question2=input("а этот?")
+# print("это  стоит-",question2)
+
+# print("я беру оба, сколько будет стоить?",int(question1)+int(question2))
+
+
+#swdwdwdwdw
+# 
+# 
+# 
+# num1=10
+# num3="err"
+# num2=2
+# dif= num1+num2
+# print(dif)
+# dif2=num1-num3
+# print(dif2)
+family = input('What is your father job?')
+print('Your father job is', family)
+family2 = input('What is your mother job?')
+print('Your mother job is', family2)
