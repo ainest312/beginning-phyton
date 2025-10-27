6d5cc6041820fd9e9546768d80a6dadb1fd53c9f
ainest312 <ainest.312@icloud.com>
2025-10-27 20:55:54 +0600

    deded

---
+++ lessen4.py
@@ -0,0 +1,60 @@
+import math
+# num = math.sqrt(121)
+# print(num)
+
+# print(max(2,3,455,5,6,443,234,545))
+# print(min(1212, 323,5654,2,5,74,345,756,1,-12))
+
+# l=(int(input()))
+# w=(int(input()))
+# h=(int(input()))
+# v=l*w*h
+
+# pi=3.14
+# r=(int(input()))
+# v1=(pi*r**2*h)/3
+# v2=pi*r**2*h
+# v3=4/3*pi*r**3
+# print(v)
+# print(v1)
+# print(v2)
+# print(v3)
+
+
+
+
+
+
+numbers=[1,2,3,4,5]
+people=["tom",'sam', 'rus','pen','oil']
+
+# people[1]="tom"
+# print(people[1]*10)
+# numbers[0]=5
+# print(numbers[0])
+
+
+# print(len(people))
+# print(people)
+# print(len(people))
+# people.append('mmm')
+# print(people)
+# print(len(people))
+
+# people1=people[ ::-1 ]
+# print(people1)
+# people[3: ]="dedede",'edede'
+# print(people)
+# people.insert(-1,"dded")
+# print(people)
+
+# people.extend(numbers)
+# print(people)
+# people.remove(people[1])
+# print(people)
+# people.clear()
+# print(people)
+oil_index=people.index("oil")
+print(oil_index)
+people[oil_index]="rererer"
+print(people)
--- second.py
+++ second.py
@@ -1,3 +1,5 @@
-a=23
-print("без ф строки ", a,"grrgrgr")
-print(f"с сторокой ф dedede {a}")
+# a=23
+# print("без ф строки ", a,"grrgrgr")
+# print(f"с сторокой ф dedede {a}")
+#  import math
+#  print('frfrfrf')
