4b63ab59d87ed3238c6e23e15ea310f83e95da16
ainest312 <ainest.312@icloud.com>
2025-10-29 20:59:07 +0600

    4

--- lessen4.py
+++ lessen4.py
@@ -1,4 +1,4 @@
-import math
+# import math
 # num = math.sqrt(121)
 # print(num)
 
@@ -25,8 +25,8 @@ import math
 
 
 
-numbers=[1,2,3,4,5]
-people=["tom",'sam', 'rus','pen','oil']
+# numbers=[1,2,3,4,5]
+# people=["tom",'sam', 'rus','pen','oil']
 
 # people[1]="tom"
 # print(people[1]*10)
@@ -54,9 +54,114 @@ people=["tom",'sam', 'rus','pen','oil']
 # print(people)
 # people.clear()
 # print(people)
-oil_index=people.index("oil")
-print(oil_index)
-people[oil_index]="rererer"
-print(people)
-print(people)
+# oil_index=people.index("oil")
+# print(oil_index)
+# people[oil_index]="rererer"
+# print(people)
+# print(people)
+ 
+# text="hallo my name is Sam"
+# list1=[2,3,4,5,'name']
+# search="name"
+# if search in text:
+#     print(f'{search} is in text')
+# if search in list1:
+#     print(f"{search} is in list1")
+# num=int(input())
+# if num/2:
+#     print("четное")
+# else:
+#     print("не четное")
+
+#1
+# num=int(input())
+# if num>0:
+#     print(f'число-{num} является положительным')
+# else:
+#     print(f"число-{num} является отрицательным")
+#     if num==0:
+#         print(f'число-{num} ровна 0')
+
+#2 
+# num=int(input())
+# if num%2==0:
+#     print(f'Число {num} является четным')
+# else:
+#     print(f"Число {num} является  не четным")
+
+#3
+# print("введите число a=" )
+# a=int(input())
+# print("введите число b=" )
+# b=int(input())
+# if a>2 and b<=3:
+#     print("Справедливы неравенства A > 2 и B ≤ 3».")
+# else:
+#     print("не справедливы неравенства A > 2 и B ≤ 3».")
+
+
+#4
+# print("введите число a=" )
+# a=int(input())
+# print("введите число b=" )
+# b=int(input())
+# if a>=0 or b<-2:
+#     print(f'Справедливы неравенства A ≥ 0 или B < −2')
+#     if   b>=-2:
+#         print('Справедливы неравенства A ≥ 0 или B >=−2')
+#         if  a<=0:
+#             print("Справедливы неравенства A <= 0 или B <=−2")
+       
+# else:
+#     print('не справедливы неравенства A ≥ 0 или B < −2')
+ 
+    
+# text="rustaaaam efefef. efefefef"
+# list1="milk"
+# new_text=text.upper()
+# new_text=text.lower()
+# new_text=text.capitalize()
+# new_text=list1.replace("k", 'ch')
+# name=text.split()
+# print(name)
+
+
+
+    
+#1
+# a=32
+# b=3.5
+# x=True
+# new=str(a)
+# new1=str(b)
+# new2=str(x)
+
+# print(type(new))
+#2
+# text=str(input())
+# print(text)
+
+#3
+
+# print("Write your name:",name=input())
+# print("Write your surname",surname=input())
+
+
+#4
+
+# print("Write your name:")
+# name=input()
+# print("Write your surname")
+# surname=input()
+# new=name+surname
+# print(new)
+
+
+#5
+name=input()
+surname=input()
+print(f'Hello {name} Your score is {surname} ')
+
+
+
 
